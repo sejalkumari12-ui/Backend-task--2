272241b1772572fbffa5a28436300c12f30f4247
sejalkumari12-ui <sejalkumari5679@gmail.com>
2026-02-06 08:50:19 +0530

    notes created

--- src/app.js
+++ src/app.js
@@ -6,5 +6,15 @@ app.use(express.json())
 
 const notes= []
 
+/*POST /notes */
+app.post("/notes",(req,res)=>{
+   notes.push(req.body)
+
+   res.status(201).json({
+    message:"notes created"
+   })
+
+})
+
 
 module.exports = app
